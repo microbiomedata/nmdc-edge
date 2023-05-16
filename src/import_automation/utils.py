476ffe3dd70864c19ba9c3b5d95245461745a174
Shane Canon <scanon@lbl.gov>
2023-05-16 11:30:41 -0700

    fixed how multiple objects are handled

--- src/import_automation/utils.py
+++ src/import_automation/utils.py
@@ -1,19 +1,22 @@
 from zipfile import ZipFile
+from typing import Union, List
 import logging
 import os
 
 logger = logging.getLogger(__name__) 
 
 
-def object_action(file_s,action,multiple=False):
+def object_action(file_s,action, activity_id, nmdc_suffix,activity_dir=None, multiple=False):
     
     if action == 'none':
         return get_basename(file_s)
     elif action == 'rename':
-        return rename(file_s)
+        return rename(activity_id, nmdc_suffix)
     elif action == 'zip':
         if multiple == True:
-            map(zip_file, file_s)
+            for file in file_s:
+                zip_name = zip_file(activity_id, nmdc_suffix, file, activity_dir)
+            return zip_name
         else:
             zip_file(file_s)
     else:
@@ -40,34 +43,23 @@ def zip_file(activity_id, nmdc_suffix,file,project_dir):
     zip_file_name = rename(activity_id, nmdc_suffix)
     
     if not os.path.exists(os.path.join(project_dir,zip_file_name)):
+        os.makedirs(project_dir)
         with ZipFile(os.path.join(project_dir, zip_file_name), mode='w') as zipped_file:
             zipped_file.write(file)
-            
     else:
         with ZipFile(os.path.join(project_dir, zip_file_name), mode='a') as zipped_file:
             zipped_file.write(file)
             
     return zip_file_name
 
-    
-
-def file_handler(project_dir,original_file, updated_file, destination_dir):
-    try:
-        os.makedirs(destination_dir)
-    except FileExistsError:
-        logger.debug(f'{destination_dir} already exists')
-    
-    original_path = os.path.join(project_dir,original_file)
-    linked_path = os.path.join(destination_dir,updated_file)
-    try:
-        os.link(original_path, linked_path)
-    except FileExistsError:
-        logger.info(f'{linked_path} already exists')
         
-    return linked_path
+def file_link(import_project_dir: str,import_file: Union[str, List[str]], destination_dir: str, updated_file: str):
     
-def file_link(import_project_dir: str,import_file: str, destination_dir: str, updated_file: str):
-        
+    if type(import_file) == list:
+        print("Object has already been linked in objection specific import action")
+        return os.path.join(destination_dir, updated_file)
+    
+    elif type(import_file) == str:    
         try:
             os.makedirs(destination_dir)
         except FileExistsError:
@@ -81,3 +73,5 @@ def file_link(import_project_dir: str,import_file: str, destination_dir: str, up
             os.link(original_path, linked_path)
         except FileExistsError:
             logger.info(f'{linked_path} already exists')
+                
+        return linked_path
