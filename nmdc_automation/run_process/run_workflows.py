36b2447db9c448adca31cb39cbcdc8ea70c87e97
Michal Babinski <mbabinski@lanl.gov>
2023-05-24 11:06:55 -0700

    change cli sctructure

--- nmdc_automation/run_process/run_workflows.py
+++ nmdc_automation/run_process/run_workflows.py
@@ -6,84 +6,91 @@ import sys
 from nmdc_automation.workflow_automation.watch_nmdc_dev import Watcher
 
 
+
 @click.group()
 def cli():
     pass
 
-@cli.command()
-@click.argument('site_configuration_file', type=click.Path(exists=True))
-def watcher(site_configuration_file):
+@cli.group()
+@click.option('-config','--config', 'site_configuration_file', type=click.Path(exists=True), required=True)
+@click.pass_context
+def watcher(ctx, site_configuration_file):
     
     logging_level = os.getenv('NMDC_LOG_LEVEL', logging.INFO)
     logging.basicConfig(level=logging_level, format='%(asctime)s %(levelname)s: %(message)s')
     logger = logging.getLogger(__name__)
     
-    watcher = Watcher(site_configuration_file)
+    ctx.obj = Watcher(site_configuration_file)
 
-    @click.command()
-    @click.argument('job_ids', nargs=-1)
-    def submit(job_ids):
-        watcher.restore()
-        for job_id in job_ids:
-            job = watcher.nmdc.get_job(job_id)
-            claims = job['claims']
-            if not claims:
-                logger.error("todo: Handle Empty Claims")
-                sys.exit(1)
-            else:
-                opid = claims[0]['op_id']
-                job = watcher.find_job_by_opid(opid)
-                if job:
-                    print(f"{job_id} use resubmit")
-                    continue
-            watcher.submit(job, opid, force=True)
-            watcher.job_checkpoint()
-
-    @click.command()
-    @click.argument('activity_ids', nargs=-1)
-    def resubmit(activity_ids):
-        watcher.restore()
-        for act_id in activity_ids:
-            job = None
-            if act_id.startswith('nmdc:sys'):
-                key = 'opid'
-            else:
-                key = 'activity_id'
-            for found_job in watcher.jobs:
-                job_record = found_job.get_state()
-                if job_record[key] == act_id:
-                    job = found_job
-                    break
-            if not job:
-                print(f"No match found for {act_id}")
+@watcher.command()
+@click.pass_context
+@click.argument('job_ids', nargs=-1)
+def submit(ctx,job_ids):
+    watcher = ctx.obj
+    watcher.restore()
+    for job_id in job_ids:
+        job = watcher.nmdc.get_job(job_id)
+        claims = job['claims']
+        if not claims:
+            print("todo: Handle Empty Claims")
+            sys.exit(1)
+        else:
+            opid = claims[0]['op_id']
+            job = watcher.find_job_by_opid(opid)
+            if job:
+                print(f"{job_id} use resubmit")
                 continue
-            if job.last_status in ["Running", "Submitted"]:
-                print(f"Skipping {act_id}, {job.last_status}")
-                continue
-            job.cromwell_submit(force=True)
-            watcher.ckpt()
+        watcher.submit(job, opid, force=True)
+        watcher.job_checkpoint()
+
+@watcher.command()
+@click.pass_context
+@click.argument('activity_ids', nargs=-1)
+def resubmit(ctx,activity_ids):
+    watcher = ctx.obj
+    watcher.restore()
+    for act_id in activity_ids:
+        job = None
+        if act_id.startswith('nmdc:sys'):
+            key = 'opid'
+        else:
+            key = 'activity_id'
+        for found_job in watcher.jobs:
+            job_record = found_job.get_state()
+            if job_record[key] == act_id:
+                job = found_job
+                break
+        if not job:
+            print(f"No match found for {act_id}")
+            continue
+        if job.last_status in ["Running", "Submitted"]:
+            print(f"Skipping {act_id}, {job.last_status}")
+            continue
+        job.cromwell_submit(force=True)
+        watcher.ckpt()
 
 
-    @click.command()
-    def sync():
-        watcher.restore()
-        watcher.update_op_state_all()
+@watcher.command()
+@click.pass_context
+def sync(ctx):
+    watcher = ctx.obj
+    watcher.restore()
+    watcher.update_op_state_all()
 
-    @click.command()
-    def daemon():
-        watcher.watch()
+@watcher.command()
+@click.pass_context
+def daemon(ctx):
+    watcher = ctx.obj
+    watcher.watch()
 
-    @click.command()
-    @click.argument('opid')
-    def reset(opid):
-        print(watcher.nmdc.update_op(opid, done=False))
-
-    cli.add_command(submit)
-    cli.add_command(resubmit)
-    cli.add_command(sync)
-    cli.add_command(daemon)
-    cli.add_command(reset)
-
+@watcher.command()
+@click.pass_context
+@click.argument('opid')
+def reset(ctx,opid):
+    watcher = ctx.obj
+    print(watcher.nmdc.update_op(opid, done=False))
 
 if __name__ == '__main__':
     cli()
+
+    
