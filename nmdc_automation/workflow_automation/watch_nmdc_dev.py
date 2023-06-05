de14cbefdf383736e7e4d2706ebf1df0fe65d903
Michal Babinski <mbabinski@lanl.gov>
2023-06-05 11:40:53 -0700

    refactored entire watcher class
    refactored and extracted alll functions in the watcher class to make it more readable and susinct.

--- nmdc_automation/workflow_automation/watch_nmdc_dev.py
+++ nmdc_automation/workflow_automation/watch_nmdc_dev.py
@@ -12,7 +12,8 @@ from os.path import exists
 from nmdc_automation.api.nmdcapi import nmdcapi
 from nmdc_automation.workflow_automation.config import config
 from nmdc_automation.workflow_automation.wfutils import _md5
-from nmdc_automation.workflow_automation.wfutils import job as wfjob
+from nmdc_automation.workflow_automation.wfutils import WorkflowJob as wfjob
+from nmdc_automation.workflow_automation.wfutils import NmdcSchema as schema
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +22,7 @@ class Watcher:
     def __init__(self, site_configuration_file):
         self._POLL = 20
         self._MAX_FAILS = 2
+        self.should_skip_claim = bool(os.environ.get("SKIP_CLAIM"))
         self.config = config(site_configuration_file)
         self.client_id = self.config.conf['credentials']['client_id']
         self.client_secret = self.config.conf['credentials']['client_secret']
@@ -29,41 +31,50 @@ class Watcher:
         self.stage_dir = self.config.get_stage_dir()
         self.raw_dir = self.config.conf['directories']['raw_dir']
         self.jobs = []
-        self.nmdc = nmdcapi()
-        
+        self.runtime_api = nmdcapi()
         self._ALLOWED = self.config._generate_allowed_workflows()
-
-    def restore(self, nocheck=False):
+        
+   
+    #####Restoring#################
+    def restore(self, nocheck: bool = False):
         """
-        Restore from chkpt
+        Restore from checkpoint
         """
-        if not exists(self.state_file):
+        data = self._load_state_file()
+        if not data:
             return
 
+        self.jobs = self._find_jobs(data, nocheck)
+        
+    def _load_state_file(self):
+        if not exists(self.state_file):
+            return
         with open(self.state_file, 'r') as f:
-            data = loads(f.read())
-
+            return loads(f.read())
+        
+    def _find_jobs(self, data: dict, nocheck: bool):
         new_job_list = []
         seen = dict()
         for job in data['jobs']:
             job_id = job['nmdc_jobid']
             if job_id in seen:
                 continue
-            job_record = wfjob(self.config.conf,state=job, nocheck=nocheck)
+            job_record = wfjob(self.config.conf, state=job, nocheck=nocheck)
             new_job_list.append(job_record)
             seen[job_id] = True
-
-        self.jobs = new_job_list
+            
+        return new_job_list
+    #################################
 
     def job_checkpoint(self):
         jobs = [job.get_state() for job in self.jobs]
         data = {'jobs': jobs}
         with open(self.state_file, "w") as f:
-            f.write(dumps(data, indent=2))
+            json.dump(data, f, indent=2)
 
     def cycle(self):
         self.restore()
-        if not os.environ.get("SKIP_CLAIM"):
+        if not self.should_skip_claim:
             self.claim_jobs()
         self.check_status()
 
@@ -72,83 +83,74 @@ class Watcher:
         while True:
             try:
                 self.cycle()
-            except Exception as e:
-                logger.exception("Error occurred during cycle:", exc_info=True)
+            except (IOError, ValueError,TypeError, AttributeError) as e:
+                logger.exception(f"Error occurred during cycle: {e}", exc_info=True)
             sleep(self._POLL)
 
     def find_job_by_opid(self, opid):
-        for job in self.jobs:
-            if job.opid == opid:
-                return
-            
+        return next((job for job in self.jobs if job.opid == opid), None)
+                
     def submit(self, njob, opid, force=False):
         wfid = njob['workflow']['id']
         if 'object_id_latest' in njob['config']:
             logger.warning("Old record. Skipping.")
             return
+        self.create_or_use_existing_job(njob, opid, wfid)
+        self.jobs[-1].cromwell_submit(force=force)
 
+    def create_or_use_existing_job(self, njob, opid, wfid):
         job = self.find_job_by_opid(opid)
         if job:
             logger.debug("Previously cached job")
             logger.info(f"Reusing activity {job.activity_id}")
         else:
-            job = wfjob(config=self.config.conf,typ = wfid, nmdc_jobid = njob['id'], conf = njob['config'],opid = opid, activity_id=njob['config']['activity_id'])
+            job = wfjob(config=self.config.conf, typ=wfid, nmdc_jobid=njob['id'], conf=njob['config'], opid=opid, activity_id=njob['config']['activity_id'])
             self.jobs.append(job)
-
-        job.cromwell_submit(force=force)
 
     def refresh_remote_jobs(self):
         """
         Return a filtered list of nmdc jobs.
         """
         filt = {"workflow.id": {"$in": self._ALLOWED}}
-        jobs = self.nmdc.list_jobs(filt=filt)
-        # Get the jobs we know about
-        known = {}
-        for j in self.jobs:
-            known[j.nmdc_jobid] = 1
-        resp = []
-        for job in jobs:
-            job_id = job['id']
-            if job_id in known:
-                continue
-            resp.append(job)
-        return resp
+        jobs = self.runtime_api.list_jobs(filt=filt)
+        known = set(job.nmdc_jobid for job in self.jobs)
+        return [job for job in jobs if job['id'] not in known]
 
     def claim_jobs(self):
         for job in self.refresh_remote_jobs():
             job_id = job['id']
             if job.get('claims') and len(job.get('claims')) > 0:
                 continue
-            logger.debug(f"try to claim: {job_id}")
+            logger.debug(f"Trying to claim: {job_id}")
 
-            # claim job
-            claim = self.nmdc.claim_job(job_id)
+            # Claim job
+            claim = self.runtime_api.claim_job(job_id)
             if not claim['claimed']:
                 logger.debug(claim)
-                self.submit(job, claim['id'])
-                self.job_checkpoint()
+                self.submit_and_checkpoint_job(job, claim['id'])
             else:
                 # Previously claimed
                 opid = claim['detail']['id']
-                # op = self.nmdc.get_op(opid)
                 logger.info("Previously claimed.")
-                self.submit(job, opid)
-                self.job_checkpoint()
+                self.submit_and_checkpoint_job(job, opid)
+
+    def submit_and_checkpoint_job(self, job, opid):
+        self.submit(job, opid)
+        self.job_checkpoint()
 
     def _load_json(self, file_name):
         return json.loads(open(file_name).read())
 
     
     def fix_urls(self, data_object_list, actid, subdir):
-        root = self.config.conf['url_root'].rstrip('/')
+        root = self.config.conf['nmdc']['url_root']
         for data_object in data_object_list:
             file_name = data_object['url'].split('/')[-1]
             new_url = f"{root}/{actid}/{subdir}/{file_name}"
             data_object['url'] = new_url
 
     def _get_url(self, informed_by, act_id, fname):
-        root = self.config.conf['url_root'].rstrip('/')
+        root = self.config.conf['nmdc']['url_root']
         return f"{root}/{informed_by}/{act_id}/{fname}"
 
     def _get_output_dir(self, informed_by, act_id):
@@ -159,140 +161,132 @@ class Watcher:
         return outdir
 
     def post_job_done_new(self, job):
-        # Prepare the result record
-        logger.info("Running post for op %s" % (job.opid))
+        logger.info(f"Running post for op {job.opid}")
         metadata = job.get_metadata()
-        job_outs = metadata['outputs']
         informed_by = job.conf["was_informed_by"]
         act_id = job.activity_id
         outdir = self._get_output_dir(informed_by, act_id)
 
-        obj = {}
+        output_ids = self.generate_data_objects(job, metadata['outputs'], outdir, informed_by, act_id)
+        activity_inputs = [dobj['id'] for dobj in job.input_data_objects]
+
+        self.create_activity_record(job, act_id, activity_inputs, output_ids)
+        
+        self.write_metadata_if_not_exists(metadata, outdir)
+
+        nmdc_database_obj = schema.get_database_object_dump()
+
+        objf = os.path.join(outdir, "object.json")
+        with open(objf, "w") as f:
+            json.dump({}, f)
+        
+        resp = self.runtime_api.post_objects(nmdc_database_obj)
+        logger.info(f"Response: {resp}")
+
+        job.done = True
+        resp = self.runtime_api.update_op(job.opid, done=True, meta=metadata)
+
+        return resp
+
+    def generate_data_objects(self, job, job_outs, outdir, informed_by, act_id):
         output_ids = []
-        output_dos = []
-        # Generate DOs
         prefix = job.conf['input_prefix']
+        
         for product_record in job.outputs:
             outkey = f"{prefix}.{product_record['output']}"
             full_name = job_outs[outkey]
-            print(outkey, full_name)
-            fname = os.path.basename(full_name)
-            np = os.path.join(outdir, fname)
-            shutil.copyfile(full_name, np)
+            file_name = os.path.basename(full_name)
+            new_path = os.path.join(outdir, file_name)
+            shutil.copyfile(full_name, new_path)
+
             md5 = _md5(full_name)
+            file_url = self._get_url(informed_by, act_id, file_name)
             id = product_record["id"]
-            desc = product_record['description'].replace('{id}', act_id)
-            do = {
-                "id": product_record["id"],
-                "name": product_record['name'],
-                "description": desc,
-                "file_size_bytes": os.stat(full_name).st_size,
-                "type": "nmdc:DataObject",
-                "data_object_type": product_record['data_object_type'],
-                "md5_checksum": md5,
-                "url": self._get_url(informed_by, act_id, fname)
-            }
-            output_dos.append(do)
+            do = schema.make_data_object(name=product_record['name'], full_file_name=full_name, file_url=file_url, 
+                                        data_object_type=product_record['data_object_type'], dobj_id=product_record['id'], md5_sum=md5, 
+                                        description=product_record['description'], omics_id=act_id)
+
             output_ids.append(id)
 
-        # Generate Activity
+        return output_ids
+
+    def create_activity_record(self, job, act_id, activity_inputs, output_ids):
+        activity_type = job.activity_templ["type"]
         name = job.activity_templ["name"].replace("{id}", act_id)
-        act = {
-            "has_input": [],
-            "git_url": job.conf['git_repo'],
-            "version": job.conf['release'],
-            "has_output": output_ids,
-            "was_informed_by": informed_by,
-            "id": act_id,
-            "execution_resource": self.config.conf['resource'],
-            "name": name,
-            "started_at_time": job.start,
-            "type": job.activity_templ["type"],
-            "ended_at_time": job.end
-        }
-        for k, v in job.activity_templ.items():
-            if v.startswith('{outputs.'):
-                out_key = f"{prefix}.{v[9:-1]}"
-                if out_key not in job_outs:
-                    ele = out_key.split(".")
-                    map_name = ".".join(ele[0:-1])
-                    key_name = ele[-1]
-                    act[k] = job_outs[map_name][key_name]
-                else:
-                    act[k] = job_outs[out_key]
-
-        # Add input object IDs
-        for dobj in job.input_data_objects:
-            act["has_input"].append(dobj['id'])
-
-        metadata_file = os.path.join(outdir, "metadata.json")
-        if not os.path.exists(metadata_file):
-            json.dump(metadata, open(metadata_file, "w"))
-
-        obj['data_object_set'] = output_dos
-        act_set = job.conf['activity_set']
-        obj[act_set] = [act]
-        objf = os.path.join(outdir, "object.json")
-        json.dump(obj, open(objf, "w"))
-        resp = self.nmdc.post_objects(obj)
-        logger.info("response: " + str(resp))
-        job.done = True
-        resp = self.nmdc.update_op(job.opid, done=True,
-                                   meta=metadata)
-        return resp
+        schema.create_activity_record(activity_record=activity_type, activity_name=name, workflow=job.conf, activity_id=act_id, resource=self.config['site']['resource'],
+                                    has_inputs_list=activity_inputs, has_output_list=output_ids, start_time=job.start, end_time=job.end)
 
     def post_job_done(self, job):
-        # Prepare the result record
-        logger.info("Running post for op %s" % (job.opid))
-        md = job.get_metadata()
-        dd = self.config.get_data_dir()
-        obj = dict()
+        logger.info(f"Running post for op {job.opid}")
+        metadata = job.get_metadata()
+        data_dir = self.config.get_data_dir()
         informed_by = job.conf["was_informed_by"]
         subdir = os.path.join(informed_by, job.activity_id)
-        outdir = os.path.join(dd, subdir)
-        if not os.path.exists(outdir):
-            os.makedirs(outdir)
-        for k, v in md['outputs'].items():
-            np = os.path.join(outdir, os.path.basename(v))
-            if k.endswith("objects"):
-                obj = json.load(open(v))
-                # TODO Move this into the workflow
-                self.fix_urls(obj['data_object_set'], informed_by,
-                              job.activity_id)
-                if not os.path.exists(np):
-                    json.dump(obj, open(np, "w"))
-            else:
-                if os.path.exists(np):
-                    logger.warning(f"Skipping output {np} already exist")
-                    continue
-                shutil.copyfile(v, np)
-        mdf = os.path.join(outdir, "metadata.json")
-        if not os.path.exists(mdf):
-            json.dump(md, open(mdf, "w"))
+        outdir = os.path.join(data_dir, subdir)
+
+        os.makedirs(outdir, exist_ok=True)
+
+        obj = self.process_job_outputs(job, metadata, outdir)
+
+        self.write_metadata_if_not_exists(metadata, outdir)
+
         resp = self.nmdc.post_objects(obj)
-        logger.info("response: " + str(resp))
+        logger.info(f"Response: {resp}")
+
         job.done = True
-        resp = self.nmdc.update_op(job.opid, done=True,
-                                   meta=md)
+        resp = self.nmdc.update_op(job.opid, done=True, meta=metadata)
+
         return resp
+
+    def process_job_outputs(self, job, metadata, outdir):
+        obj = dict()
+        for key, value in metadata['outputs'].items():
+            new_path = os.path.join(outdir, os.path.basename(value))
+            if key.endswith("objects"):
+                with open(value) as f:
+                    obj = json.load(f)
+
+                self.fix_urls(obj['data_object_set'], job.conf["was_informed_by"], job.activity_id)
+
+                if not os.path.exists(new_path):
+                    with open(new_path, "w") as f:
+                        json.dump(obj, f)
+            else:
+                self.copy_output_if_not_exists(value, new_path)
+        return obj
+
+    def copy_output_if_not_exists(self, old_path, new_path):
+        if os.path.exists(new_path):
+            logger.warning(f"Skipping output {new_path} already exists")
+        else:
+            shutil.copyfile(old_path, new_path)
+
+    def write_metadata_if_not_exists(self, metadata, outdir):
+        metadata_filepath = os.path.join(outdir, "metadata.json")
+        if not os.path.exists(metadata_filepath):
+            with open(metadata_filepath, "w") as f:
+                json.dump(metadata, f)
 
     def check_status(self):
         for job in self.jobs:
-            if job.done:
-                continue
-            status = job.check_status()
-            if status == 'Succeeded' and job.opid and not job.done:
-                if job.outputs:
-                    self.post_job_done_new(job)
-                else:
-                    self.post_job_done(job)
-                self.job_checkpoint()
-            elif status == 'Failed' and job.opid:
-                if job.failed_count < self._MAX_FAILS:
-                    job.failed_count += 1
-                    job.cromwell_submit()
-                self.job_checkpoint()
-            elif job.opid and not job.done:
-                continue
-            
+            if not job.done:
+                status = job.check_status()
+                if status == 'Succeeded' and job.opid:
+                    self.process_successful_job(job)
+                elif status == 'Failed' and job.opid:
+                    self.process_failed_job(job)
+
+        self.job_checkpoint()
+
+
+    def process_successful_job(self, job):
+        if job.outputs:
+            self.post_job_done_new(job)
+        else:
+            self.post_job_done(job)
+
+    def process_failed_job(self, job):
+        if job.failed_count < self._MAX_FAILS:
+            job.failed_count += 1
+            job.cromwell_submit()
         
