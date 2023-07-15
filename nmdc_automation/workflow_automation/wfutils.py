786c36da13d2e2b55573b84786c1d26e95857739
Shane Canon <scanon@lbl.gov>
2023-07-15 13:50:37 -0700

    Flake8 fixes for wfutils

--- nmdc_automation/workflow_automation/wfutils.py
+++ nmdc_automation/workflow_automation/wfutils.py
@@ -6,15 +6,14 @@ import tempfile
 import requests
 import nmdc_schema.nmdc as nmdc
 import logging
-import urllib
 import datetime
 import pytz
 import hashlib
-import yaml
 from linkml_runtime.dumpers import json_dumper
 
+
 class WorkflowJob():
-    
+
     DEFAULT_STATUS = "Unsubmitted"
     SUCCESS_STATUS = "Succeeded"
     METADATA_URL_SUFFIX = "/metadata"
@@ -22,7 +21,7 @@ class WorkflowJob():
     LABEL_PARAMETERS = ['release', 'wdl', 'git_repo']
     CHUNK_SIZE = 1000000  # 1 MB
     GIT_RELEASES_PATH = "/releases/download"
-    
+
     debug = False
     dryrun = False
     options = None
@@ -32,7 +31,9 @@ class WorkflowJob():
     start = None
     end = None
 
-    def __init__(self, site_config, typ=None, workflow_config=None,nmdc_jobid=None, opid=None, activity_id=None, state=None, nocheck=False):
+    def __init__(self, site_config, typ=None, workflow_config=None,
+                 nmdc_jobid=None, opid=None, activity_id=None,
+                 state=None, nocheck=False):
         self.config = site_config
         self.workflow_config = workflow_config
         self.set_config_attributes()
@@ -51,7 +52,8 @@ class WorkflowJob():
     def load_workflow_config(self):
         self.outputs = self.workflow_config.get('outputs')
         self.activity_templ = self.workflow_config.get('activity')
-        self.input_data_objects = self.workflow_config.get('input_data_objects')
+        self.input_data_objects = \
+            self.workflow_config.get('input_data_objects')
 
     def set_initial_state(self, state, activity_id, typ, nmdc_jobid, opid):
         if state:
@@ -82,7 +84,7 @@ class WorkflowJob():
         self.jobid = None
         self.failed_count = 0
         self.last_status = self.DEFAULT_STATUS
-        
+
     def get_state(self):
         data = {
             "type": self.type,
@@ -98,7 +100,7 @@ class WorkflowJob():
             "opid": self.opid
             }
         return data
-    
+
     def check_status(self):
         """
         Check the status in Cromwell
@@ -111,9 +113,9 @@ class WorkflowJob():
 
         try:
             resp = requests.get(url)
-            resp.raise_for_status() 
-        except requests.exceptions.RequestException as e:
-            # logging.error(f"Error checking status: {e}")
+            resp.raise_for_status()
+        except requests.exceptions.RequestException as ex:
+            logging.error(f"Error checking status: {ex}")
             self.last_status = "Error"
             return self.last_status
 
@@ -125,7 +127,7 @@ class WorkflowJob():
             self.end = datetime.datetime.now(pytz.utc).isoformat()
 
         return state
-        
+
     def get_metadata(self):
         """
         Check the status in Cromwell
@@ -134,9 +136,9 @@ class WorkflowJob():
             return self.DEFAULT_STATUS
         url = f"{self.cromurl}/{self.jobid}{self.METADATA_URL_SUFFIX}"
         resp = requests.get(url)
-        resp.raise_for_status() 
+        resp.raise_for_status()
         return resp.json()
-    
+
     def json_log(self, data, title="json_log"):
         logging.debug(title)
         logging.debug(json.dumps(data, indent=2))
@@ -161,30 +163,31 @@ class WorkflowJob():
         return labels
 
     def get_label_parameters(self):
-        return {param: self.workflow_config[param] for param in self.LABEL_PARAMETERS}
-    
+        return {param: self.workflow_config[param]
+                for param in self.LABEL_PARAMETERS}
+
     def fetch_release_file(self, fn, suffix=None):
         release = self.workflow_config['release']
         base_url = self.workflow_config['git_repo'].rstrip('/')
         url = base_url + f"{self.GIT_RELEASES_PATH}/{release}/{fn}"
 
-        print(f"BASE URL: {base_url}")
-        print(f"URL: {url}")
-        
+        logging.debug(f"BASE URL: {base_url}")
+        logging.debug(f"URL: {url}")
+
         resp = requests.get(url, stream=True)
-        resp.raise_for_status() 
+        resp.raise_for_status()
 
         fp, fname = tempfile.mkstemp(suffix=suffix)
         try:
             with os.fdopen(fp, 'wb') as fd:
                 for chunk in resp.iter_content(chunk_size=self.CHUNK_SIZE):
                     fd.write(chunk)
-        except:
-            os.unlink(fname) 
-            raise  
+        except Exception as ex:
+            os.unlink(fname)
+            raise ex
 
         return fname
-    
+
     def generate_files(self, conf):
         wdl_file = self.fetch_release_file(conf['wdl'], suffix='.wdl')
         bundle_file = self.fetch_release_file("bundle.zip", suffix='.zip')
@@ -216,9 +219,9 @@ class WorkflowJob():
 
             job_id = "unknown"
             if not self.dryrun:
-                print(self.cromurl)
+                logging.debug(self.cromurl)
                 resp = requests.post(self.cromurl, data={}, files=files)
-                resp.raise_for_status() 
+                resp.raise_for_status()
                 data = resp.json()
                 self.json_log(data, title="Response")
                 job_id = data['id']
@@ -236,13 +239,16 @@ class WorkflowJob():
 
 
 class NmdcSchema():
-    
+
     def __init__(self):
         self.nmdc_db = nmdc.Database()
         self._data_object_string = "nmdc:DataObject"
         self.activity_store = self.activity_map()
-        
-    def make_data_object(self,name: str,full_file_name: str,file_url: str, data_object_type: str, dobj_id: str, md5_sum: str,description: str, omics_id: str) -> None:
+
+    def make_data_object(self, name: str, full_file_name: str,
+                         file_url: str, data_object_type: str,
+                         dobj_id: str, md5_sum: str,
+                         description: str, omics_id: str) -> None:
         """Create nmdc database data object
 
         Args:
@@ -255,7 +261,7 @@ class NmdcSchema():
             description (str): description for data object
             omics_id (str): minted omics id
         """
-        
+
         self.nmdc_db.data_object_set.append(
             nmdc.DataObject(
                 file_size_bytes=os.stat(full_file_name).st_size,
@@ -264,21 +270,21 @@ class NmdcSchema():
                 data_object_type=data_object_type,
                 type=self._data_object_string,
                 id=dobj_id,
-                md5_checksum=md5_sum, 
+                md5_checksum=md5_sum,
                 description=description.replace("{id}", omics_id)
             ))
-                    
-    def create_activity_record(self, activity_record, activity_name, workflow, activity_id, resource, has_inputs_list, has_output_list, omic_id, start_time, end_time):
-        
-        
-        
+
+    def create_activity_record(self, activity_record, activity_name,
+                               workflow, activity_id, resource,
+                               has_inputs_list, has_output_list,
+                               omic_id, start_time, end_time):
         database_activity_set = self.activity_store[activity_record][0]
-                
+
         database_activity_range = self.activity_store[activity_record][1]
-        
+
         database_activity_set.append(
                     database_activity_range(
-                        id=activity_id, #call minter for activity type
+                        id=activity_id,  # call minter for activity type
                         name=activity_name,
                         git_url=workflow['git_repo'],
                         version=workflow['release'],
@@ -286,26 +292,35 @@ class NmdcSchema():
                         execution_resource=resource,
                         started_at_time=start_time,
                         has_input=has_inputs_list,
-                        has_output=has_output_list, 
+                        has_output=has_output_list,
                         type=activity_record,
-                        ended_at_time=end_time, 
-                        was_informed_by=omic_id, 
+                        ended_at_time=end_time,
+                        was_informed_by=omic_id,
                     ))
-        
+
     def activity_map(self):
-        '''Inform Object Mapping Process what activies need to be imported and distrubuted across the process'''
-        
+        '''
+        Inform Object Mapping Process what activies need to be imported and
+        distrubuted across the process
+        '''
+
         activity_store_dict = {
-            'nmdc:MetagenomeSequencing': (self.nmdc_db.metagenome_sequencing_activity_set, nmdc.MetagenomeSequencingActivity),
-            'nmdc:ReadQcAnalysisActivity': (self.nmdc_db.read_qc_analysis_activity_set,nmdc.ReadQcAnalysisActivity),
-            'nmdc:ReadBasedTaxonomyAnalysisActivity': (self.nmdc_db.read_based_taxonomy_analysis_activity_set, nmdc.ReadBasedTaxonomyAnalysisActivity),
-            'nmdc:MetagenomeAssembly': (self.nmdc_db.metagenome_assembly_set, nmdc.MetagenomeAssembly),
-            'nmdc:MetagenomeAnnotationActivity': (self.nmdc_db.metagenome_annotation_activity_set, nmdc.MetagenomeAnnotationActivity),
-            'nmdc:MAGsAnalysisActivity': (self.nmdc_db.mags_activity_set, nmdc.MagsAnalysisActivity)
+            'nmdc:MetagenomeSequencing': (self.nmdc_db.metagenome_sequencing_activity_set,
+                                          nmdc.MetagenomeSequencingActivity),
+            'nmdc:ReadQcAnalysisActivity': (self.nmdc_db.read_qc_analysis_activity_set,
+                                            nmdc.ReadQcAnalysisActivity),
+            'nmdc:ReadBasedTaxonomyAnalysisActivity': (self.nmdc_db.read_based_taxonomy_analysis_activity_set,
+                                                       nmdc.ReadBasedTaxonomyAnalysisActivity),
+            'nmdc:MetagenomeAssembly': (self.nmdc_db.metagenome_assembly_set,
+                                        nmdc.MetagenomeAssembly),
+            'nmdc:MetagenomeAnnotationActivity': (self.nmdc_db.metagenome_annotation_activity_set,
+                                                  nmdc.MetagenomeAnnotationActivity),
+            'nmdc:MAGsAnalysisActivity': (self.nmdc_db.mags_activity_set,
+                                          nmdc.MagsAnalysisActivity)
         }
-                
+
         return activity_store_dict
-    
+
     def get_database_object_dump(self):
         """
         Get the NMDC database object.
@@ -313,8 +328,10 @@ class NmdcSchema():
         Returns:
             nmdc.Database: NMDC database object.
         """
-        nmdc_database_object = json_dumper.dumps(self.nmdc_db,inject_type=False)
+        nmdc_database_object = json_dumper.dumps(self.nmdc_db,
+                                                 inject_type=False)
         return nmdc_database_object
+
 
 def _json_tmp(data):
     fp, fname = tempfile.mkstemp(suffix='.json')
@@ -322,8 +339,10 @@ def _json_tmp(data):
         fd.write(json.dumps(data))
     return fname
 
+
 def jprint(obj):
     print(json.dumps(obj, indent=2))
-    
+
+
 def _md5(file):
     return hashlib.md5(open(file, 'rb').read()).hexdigest()
