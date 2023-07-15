d0a52dcab9b4aa336f5d87b176101c1609408fde
Shane Canon <scanon@lbl.gov>
2023-07-15 13:39:06 -0700

    Flake8 fixes to import classes
    This cleans up the flake8 issues other than long lines.

--- nmdc_automation/import_automation/activity_mapper.py
+++ nmdc_automation/import_automation/activity_mapper.py
@@ -1,7 +1,5 @@
 import os
-import sys
 import re
-import csv
 import logging
 import datetime
 import pytz
@@ -12,13 +10,14 @@ from linkml_runtime.dumpers import json_dumper
 from nmdc_automation.api.nmdcapi import nmdcapi
 from nmdc_automation.import_automation.utils import object_action, file_link, get_md5
 
-logger = logging.getLogger(__name__) 
+logger = logging.getLogger(__name__)
 runtime = nmdcapi()
 
+
 class GoldMapper():
-    
+
     def __init__(self, file_list: List[str], omics_id: str, yaml_file: str, project_directory: str):
-        
+
         """
         Initialize the GoldMapper object.
 
@@ -29,51 +28,52 @@ class GoldMapper():
             root_directory: Root directory path.
             project_directory: Project directory path.
         """
-        
+
         with open(yaml_file, 'r') as file:
             self.import_data = yaml.safe_load(file)
-        
+
         self.nmdc_db = nmdc.Database()
         self.file_list = file_list
         self.omics_id = omics_id
-        self.root_dir = os.path.join(self.import_data['Workflow Metadata']['Root Directory'],omics_id)
+        self.root_dir = os.path.join(self.import_data['Workflow Metadata']['Root Directory'], omics_id)
         self.project_dir = project_directory
         self.url = self.import_data['Workflow Metadata']['Source URL']
         self.data_object_type = "nmdc:DataObject"
         self.objects = {}
         self.activity_store = self.activity_imports()
-        
-    
+
     def unique_object_mapper(self) -> None:
-        
+
         """
         Map unique data objects from the file list based on unique matching import suffix.
         The method relates each object to an activity ID and updates the file with object action.
         It updates the nmdc database with the DataObject and stores the information in the objects dictionary.
         """
-        
+
         for data_object_dict in self.import_data['Data Objects']['Unique']:
             for file in self.file_list:
-            
-                if data_object_dict == None:
+
+                if data_object_dict is None:
                     continue
-                
-                elif re.search(data_object_dict['import_suffix'] ,file, re.IGNORECASE):
-                        
+                elif 'import_suffix' not in data_object_dict:
+                    logging.warning("Missing suffix")
+                    continue
+                elif re.search(data_object_dict['import_suffix'], file, re.IGNORECASE):
+
                     activity_id = self.relate_object_by_activity_id(data_object_dict['output_of'])
-                    
+
                     file_destination_name = object_action(file, data_object_dict['action'], activity_id, data_object_dict['nmdc_suffix'])
-                    
-                    activity_dir = os.path.join(self.root_dir,activity_id)
-                    
+
+                    activity_dir = os.path.join(self.root_dir, activity_id)
+
                     updated_file = file_link(self.project_dir, file, activity_dir, file_destination_name)
-                    
+
                     filemeta = os.stat(updated_file)
-                    
+
                     md5 = get_md5(updated_file)
-                    
+
                     dobj = runtime.minter(self.data_object_type)
-                    
+
                     self.nmdc_db.data_object_set.append(
                         nmdc.DataObject(
                             file_size_bytes=filemeta.st_size,
@@ -82,43 +82,41 @@ class GoldMapper():
                             data_object_type=data_object_dict['data_object_type'],
                             type=self.data_object_type,
                             id=dobj,
-                            md5_checksum=md5, 
+                            md5_checksum=md5,
                             description=data_object_dict['description'].replace("{id}", self.omics_id)
-                    ))
-                    
+                            ))
+
                     self.objects[data_object_dict['data_object_type']] = (data_object_dict['input_to'], [data_object_dict['output_of']], dobj)
-            
+
     def multiple_objects_mapper(self) -> None:
-        
+
         """
         Maps multiple data objects from the file list based on matching import suffix into one nmdc data object.
         The method relates each object to an activity ID and updates the file with object action.
         It updates the nmdc database with the DataObject and stores the information in the objects dictionary.
         """
-        
+
         multiple_objects_list = []
-        
+
         for data_object_dict in self.import_data['Data Objects']['Multiples']:
             for file in self.file_list:
-                if re.search(data_object_dict['import_suffix'] ,file, re.IGNORECASE):
+                if re.search(data_object_dict['import_suffix'], file, re.IGNORECASE):
                     multiple_objects_list.append(file)
-                    
+
             activity_id = self.relate_object_by_activity_id(data_object_dict['output_of'])
-            
-            activity_dir = os.path.join(self.root_dir,activity_id)
-            
+
+            activity_dir = os.path.join(self.root_dir, activity_id)
+
             file_destination_name = object_action(multiple_objects_list, data_object_dict['action'], activity_id, data_object_dict['nmdc_suffix'], activity_dir=activity_dir, multiple=True)
-            
-            
+
             updated_file = file_link(self.project_dir, multiple_objects_list, activity_dir, file_destination_name)
-            
-            
+
             filemeta = os.stat(updated_file)
-            
+
             md5 = get_md5(updated_file)
-            
+
             dobj = runtime.minter(self.data_object_type)
-            
+
             self.nmdc_db.data_object_set.append(
                 nmdc.DataObject(
                     file_size_bytes=filemeta.st_size,
@@ -127,42 +125,41 @@ class GoldMapper():
                     data_object_type=data_object_dict['data_object_type'],
                     type=self.data_object_type,
                     id=dobj,
-                    md5_checksum=md5, 
+                    md5_checksum=md5,
                     description=data_object_dict['description'].replace("{id}", self.omics_id)
-            ))
-            
-            self.objects[data_object_dict['data_object_type']] = (data_object_dict['input_to'], [data_object_dict['output_of']],dobj)
+                ))
 
+            self.objects[data_object_dict['data_object_type']] = (data_object_dict['input_to'], [data_object_dict['output_of']], dobj)
 
     def activity_mapper(self) -> None:
-        
+
         """
-        Maps activities from the import data to the NMDC database. 
-        The function creates a database activity set for each workflow type in the import data, 
+        Maps activities from the import data to the NMDC database.
+        The function creates a database activity set for each workflow type in the import data,
         attaching the relevant input and output objects. It also provides other metadata for each activity.
 
-        This method assumes that the import data includes a 'Workflows' section with each workflow having 
-        a 'Type', 'Git_repo', and 'Version'. It also assumes that the import data includes a 'Workflow Metadata' 
+        This method assumes that the import data includes a 'Workflows' section with each workflow having
+        a 'Type', 'Git_repo', and 'Version'. It also assumes that the import data includes a 'Workflow Metadata'
         section with an 'Execution Resource'.
         """
-        
+
         for workflow in self.import_data['Workflows']:
             if workflow['Type'] in self.activity_store:
-                
+
                 has_inputs_list, has_output_list = self.attach_objects_to_activity(workflow['Type'])
-                #quick fix because nmdc-schema does not support [], even though raw product has none
+                # quick fix because nmdc-schema does not support [], even though raw product has none
                 if len(has_inputs_list) == 0:
                     has_inputs_list = ['None']
-                    
+
                 database_activity_set = self.activity_store[workflow['Type']][0]
-                
+
                 database_activity_range = self.activity_store[workflow['Type']][1]
-                
+
                 activity_id = self.activity_store[workflow['Type']][2]
-                
+
                 database_activity_set.append(
                     database_activity_range(
-                        id=activity_id, #call minter for activity type
+                        id=activity_id,  # call minter for activity type
                         name=workflow['Activity']['name'].replace("{id}", activity_id),
                         git_url=workflow['Git_repo'],
                         version=workflow['Version'],
@@ -170,90 +167,88 @@ class GoldMapper():
                         execution_resource=self.import_data['Workflow Metadata']['Execution Resource'],
                         started_at_time=datetime.datetime.now(pytz.utc).isoformat(),
                         has_input=has_inputs_list,
-                        has_output=has_output_list, 
+                        has_output=has_output_list,
                         type=workflow['Type'],
-                        ended_at_time=datetime.datetime.now(pytz.utc).isoformat(), 
-                        was_informed_by=self.omics_id, 
+                        ended_at_time=datetime.datetime.now(pytz.utc).isoformat(),
+                        was_informed_by=self.omics_id,
                     ))
-                    
+
     def activity_imports(self) -> Dict[str, Tuple[Callable, Callable, str]]:
         '''Inform Object Mapping Process what activies need to be imported and distrubuted across the process'''
-        
+
         activity_store_dict = {
             'nmdc:MetagenomeSequencing': (self.nmdc_db.metagenome_sequencing_activity_set, nmdc.MetagenomeSequencingActivity, runtime.minter("nmdc:MetagenomeSequencingActivity")),
-            'nmdc:ReadQcAnalysisActivity': (self.nmdc_db.read_qc_analysis_activity_set,nmdc.ReadQcAnalysisActivity, runtime.minter("nmdc:ReadQcAnalysisActivity")),
+            'nmdc:ReadQcAnalysisActivity': (self.nmdc_db.read_qc_analysis_activity_set, nmdc.ReadQcAnalysisActivity, runtime.minter("nmdc:ReadQcAnalysisActivity")),
             'nmdc:ReadBasedTaxonomyAnalysisActivity': (self.nmdc_db.read_based_taxonomy_analysis_activity_set, nmdc.ReadBasedTaxonomyAnalysisActivity, runtime.minter("nmdc:ReadBasedTaxonomyAnalysisActivity")),
             'nmdc:MetagenomeAssembly': (self.nmdc_db.metagenome_assembly_set, nmdc.MetagenomeAssembly, runtime.minter("nmdc:MetagenomeAssembly")),
             'nmdc:MetagenomeAnnotationActivity': (self.nmdc_db.metagenome_annotation_activity_set, nmdc.MetagenomeAnnotationActivity, runtime.minter("nmdc:MetagenomeAnnotationActivity")),
             'nmdc:MAGsAnalysisActivity': (self.nmdc_db.mags_activity_set, nmdc.MagsAnalysisActivity, runtime.minter("nmdc:MagsAnalysisActivity"))
         }
-        
-        
+
         for activity_config in self.import_data['Workflows']:
-            if activity_config['Import'] == False:
+            if not activity_config['Import']:
                 del activity_store_dict[activity_config['Type']]
-                
+
         return activity_store_dict
-        
+
     def relate_object_by_activity_id(self, output_of: str) -> str:
         '''Map data object to activity type and returns minted activity id
-        
+
         Args:
             output_of (str): The activity type the data object is an output of.
 
         Returns:
             str: The activity id that the data object is an output of.
         '''
-        
-        
+
         return self.activity_store[output_of][2]
-    
-    def attach_objects_to_activity(self, activity_type:str) -> Tuple[List[str], List[str]]:
+
+    def attach_objects_to_activity(self, activity_type: str) -> Tuple[List[str], List[str]]:
         """
         Get data objects that inform activity inputs and outputs.
 
         This function iterates through the stored objects, checking if the provided activity_type
-        is in the 'input_to' or 'output_of' fields. If it is, the corresponding object is appended 
+        is in the 'input_to' or 'output_of' fields. If it is, the corresponding object is appended
         to the respective list (inputs or outputs).
 
         Args:
             activity_type (str): The type of nmdc activity to relate object to.
 
         Returns:
-            Tuple[List[str], List[str]]: Two lists containing the data object ids of the data objects that are 
-                inputs to and outputs of the specified activity type.
+            Tuple[List[str], List[str]]: Two lists containing the data object
+            ids of the data objects that are inputs to and outputs of the specified
+            activity type.
         """
-        
+
         data_object_outputs_of_list = []
-        
+
         data_object_inputs_to_list = []
-        
+
         for _, data_object_items in self.objects.items():
             if activity_type in data_object_items[1]:
                 data_object_outputs_of_list.append(data_object_items[2])
             elif activity_type in data_object_items[0]:
                 data_object_inputs_to_list.append(data_object_items[2])
-                
-        
-        return data_object_inputs_to_list,data_object_outputs_of_list
-    
+
+        return data_object_inputs_to_list, data_object_outputs_of_list
+
     def post_nmdc_database_object(self) -> Dict:
-        
+
         """
         Post the nmdc database object.
 
-        This function dumps the NMDC database object into JSON format, then posts it 
-        using the runtime API.
+        This function dumps the NMDC database object into JSON format, then posts
+        it using the runtime API.
 
         Returns:
             Dict: The response from the runtime API after posting the object.
         """
-        
-        nmdc_database_object = json_dumper.dumps(self.nmdc_db,inject_type=False)
+
+        nmdc_database_object = json_dumper.dumps(self.nmdc_db,
+                                                 inject_type=False)
         res = runtime.post_objects(nmdc_database_object)
-      
         return res
-    
+
     def get_database_object_dump(self) -> nmdc.Database:
         """
         Get the NMDC database object.
@@ -261,5 +256,4 @@ class GoldMapper():
         Returns:
             nmdc.Database: NMDC database object.
         """
-        
         return self.nmdc_db
--- nmdc_automation/import_automation/utils.py
+++ nmdc_automation/import_automation/utils.py
@@ -4,9 +4,9 @@ import logging
 import hashlib
 import os
 
-logger = logging.getLogger(__name__) 
+logger = logging.getLogger(__name__)
 
-           
+
 def object_action(file_s: Union[str, List[str]], action: str, activity_id: str,
                   nmdc_suffix: str, activity_dir: str = None, multiple: bool = False) -> str:
     """
@@ -40,8 +40,8 @@ def object_action(file_s: Union[str, List[str]], action: str, activity_id: str,
             return zip_file(file_s)
     else:
         logger.error(f"No mapping action found for {file_s}")
-        
- 
+
+
 def get_basename(file: str) -> str:
     """
     Get file basename
@@ -52,12 +52,11 @@ def get_basename(file: str) -> str:
     Returns:
         str: file basename
     """
-    
-    
+
     return os.path.basename(file)
-    
-    
-def rename(activity_id:str, nmdc_suffix: str) -> str:
+
+
+def rename(activity_id: str, nmdc_suffix: str) -> str:
     """
     Renames file to target nmdc target activity name
 
@@ -68,18 +67,17 @@ def rename(activity_id:str, nmdc_suffix: str) -> str:
     Returns:
         str: nmdc file name
     """
-    
-    
-    activity_file_id = activity_id.replace(':','_' )
-    
+
+    activity_file_id = activity_id.replace(':', '_')
+
     nmdc_file_name = activity_file_id + nmdc_suffix
-    
+
     return nmdc_file_name
 
-                
-def zip_file(activity_id: str, nmdc_suffix: str, file: str,project_dir: str):
+
+def zip_file(activity_id: str, nmdc_suffix: str, file: str, project_dir: str):
     '''Add files of type Multiples to a zip file and represent as one data object
-    
+
     Args:
         activity_id (str): The activity ID associated with the object.
         nmdc_suffix (str): The NMDC suffix.
@@ -88,23 +86,23 @@ def zip_file(activity_id: str, nmdc_suffix: str, file: str,project_dir: str):
 
     Returns:
         str: Expected file name for import of Multiples as one data object.
-    
+
     '''
-    
+
     zip_file_name = rename(activity_id, nmdc_suffix)
-    
-    if not os.path.exists(os.path.join(project_dir,zip_file_name)):
+
+    if not os.path.exists(os.path.join(project_dir, zip_file_name)):
         os.makedirs(project_dir)
         with ZipFile(os.path.join(project_dir, zip_file_name), mode='w') as zipped_file:
             zipped_file.write(file)
     else:
         with ZipFile(os.path.join(project_dir, zip_file_name), mode='a') as zipped_file:
             zipped_file.write(file)
-            
+
     return zip_file_name
 
-        
-def file_link(import_project_dir: str,import_file: Union[str, List[str]], destination_dir: str, updated_file: str):
+
+def file_link(import_project_dir: str, import_file: Union[str, List[str]], destination_dir: str, updated_file: str):
     """
     Link original file to nmdc file on system path
 
@@ -117,39 +115,39 @@ def file_link(import_project_dir: str,import_file: Union[str, List[str]], destin
     Returns:
         str: os linked path of updated file
     """
-    
+
     if type(import_file) == list:
         print("Object has already been linked in objection specific import action")
         return os.path.join(destination_dir, updated_file)
-    
-    elif type(import_file) == str:    
+
+    elif type(import_file) == str:
         try:
             os.makedirs(destination_dir)
         except FileExistsError:
             logger.debug(f'{destination_dir} already exists')
-        
-            
-        original_path = os.path.join(import_project_dir,import_file)
-        linked_path = os.path.join(destination_dir,updated_file)
-        
+
+        original_path = os.path.join(import_project_dir, import_file)
+        linked_path = os.path.join(destination_dir, updated_file)
+
         try:
             os.link(original_path, linked_path)
         except FileExistsError:
             logger.info(f'{linked_path} already exists')
-                
+
         return linked_path
-    
+
+
 def get_md5(fn: str) -> str:
     """
     Generate md5 for file
-    
+
     Args:
         fn (str): file name
 
     Returns:
         md5:  md5 hash of file
         """
-        
+
     md5f = fn + '.md5'
     if os.path.exists(md5f):
         with open(md5f) as f:
