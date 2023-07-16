0d8143422f550cb1702e620c9d01c8cf559d43f8
Shane Canon <scanon@lbl.gov>
2023-07-16 11:56:21 -0700

    Refactor watch_nmdc tests
    This cleans up the watch_nmdc tests.
    * It moves some pieces into fixtures
    * Mocks the underlying requests instead of the methods so we get
      more coverage.
    * other cleanup

---
+++ tests/common.py
@@ -0,0 +1,13 @@
+from pytest import fixture
+from time import time
+
+
+@fixture(autouse=True)
+def mock_api(monkeypatch, requests_mock):
+    monkeypatch.setenv("NMDC_API_URL", "http://localhost")
+    monkeypatch.setenv("NMDC_CLIENT_ID", "anid")
+    monkeypatch.setenv("NMDC_CLIENT_SECRET", "asecret")
+    resp = {"expires": {"minutes": time()+60},
+            "access_token": "abcd"
+            }
+    requests_mock.post("http://localhost/token", json=resp)
