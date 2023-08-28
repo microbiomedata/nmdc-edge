0706c8f5e97bd2bb9ea8efc10b8f06d90dde6323
Michal Babinski <mbabinski@lanl.gov>
2023-08-28 08:16:35 -0700

    switch out _dev to prod

--- nmdc_automation/run_process/run_workflows.py
+++ nmdc_automation/run_process/run_workflows.py
@@ -3,7 +3,7 @@ from typing import List
 import logging
 import os
 import sys
-from nmdc_automation.workflow_automation.watch_nmdc_dev import Watcher
+from nmdc_automation.workflow_automation.watch_nmdc import Watcher
 
 
 
