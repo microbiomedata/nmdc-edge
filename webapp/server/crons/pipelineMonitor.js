40c6f8767ea1298e204af026e4d90496f1162bb4
Yan Xu <yxu@pn1704854.lanl.gov>
2023-09-15 14:17:56 -0600

    updated findInputsize call

--- webapp/server/crons/pipelineMonitor.js
+++ webapp/server/crons/pipelineMonitor.js
@@ -22,7 +22,7 @@ module.exports = function pipelineMonitor() {
         });
 
         //only process one request at each time
-        Project.find({ 'type': 'Metagenome Pipeline', 'status': 'in queue' }).sort({ updated: 1 }).then(projs => {
+        Project.find({ 'type': 'Metagenome Pipeline', 'status': 'in queue' }).sort({ updated: 1 }).then(async projs => {
             let proj = projs[0];
             if (!proj) {
                 logger.debug("No pipeline request to process");
@@ -35,7 +35,7 @@ module.exports = function pipelineMonitor() {
             let conf = JSON.parse(rawdata);
 
             //check input size
-            let inputsize = findInputsize(conf);
+            let inputsize = await findInputsize(conf);
             if (inputsize > process.env.MAX_CROMWELL_JOBS_INPUTSIZE) {
                 logger.debug("Project " + proj.code + " input fastq size exceeded the limit.");
                 //fail project
