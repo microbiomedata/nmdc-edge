f06d7314e5e335dd89f212c0f7e97aaa7450265e
Michael Thornton <mbthornton@lbl.gov>
2023-10-26 15:08:19 -0700

    Update rebuild_metagenome script - successfully calls queries:run endpoint

--- nmdc_automation/re_iding/scripts/rebuild_metagenome_workflow_records.py
+++ nmdc_automation/re_iding/scripts/rebuild_metagenome_workflow_records.py
@@ -6,24 +6,25 @@ rebuild_metagenome_workflow_records.py: Rebuild metagenome workflow records
 after re-ID-ing of OmicsProcessing records.
 """
 import logging
-import os
-from pathlib import Path
-# import requests
 import time
+from pathlib import Path
 
 import click
 
+from nmdc_automation.api import NmdcRuntimeUserApi
 from nmdc_automation.config import Config
-from nmdc_automation.api import NmdcRuntimeApi
 
 GOLD_STUDY_ID = "gold:Gs0114663"
 STUDY_ID = "nmdc:sty-11-aygzgv51"
 NAPA_CONFIG = Path("../../../configs/napa_config.toml")
 
+
 @click.command()
 @click.option("--study_id", default=STUDY_ID, help="Updated study ID")
-@click.option("--site_config", type=click.Path(exists=True),
-              default=NAPA_CONFIG, help="Site configuration file")
+@click.option(
+    "--site_config", type=click.Path(exists=True), default=NAPA_CONFIG,
+    help="Site configuration file"
+)
 def rebuild_workflow_records(study_id: str, site_config: bool):
     """
     Rebuild metagenome workflow records after re-ID-ing of Study, Biosample, and
@@ -44,23 +45,24 @@ def rebuild_workflow_records(study_id: str, site_config: bool):
     logging.info("starting missing_neon_soils_ecosystem_data.py...")
     logging.info(f"study_id: {study_id}")
 
-    runtime_api_client = NmdcRuntimeApi(site_config)
-
-
+    config = Config(site_config)
+    query_api_client = NmdcRuntimeUserApi(
+        username=config.napa_username, password=config.napa_password,
+        base_url=config.napa_base_url, )
 
     # 1. Retrieve all OmicsProcessing records for the given GOLD study ID
-    # https://api-napa.microbiomedata.org/omics_processing_sets?find=omics_processing_set&filter=part_of%3Agold:Gs0114663&per_page=99&page=1
-
-    params = {
-        "find": "omics_processing_set",
-        "filter": {
-            "part_of": {
-                "$elemMatch": {"$eq": study_id}
-            }
-        }
-    }
-    response = runtime_api_client.run_query(params)
-    print(response)
+    url = "queries:run"
+    params = {"find": "omics_processing_set",
+              "filter": {"part_of": {"$elemMatch": {"$eq": study_id}}}}
+    response = query_api_client.request("POST", url, params_or_json_data=params)
+    if response.status_code != 200:
+        raise Exception(
+            f"Error retrieving OmicsProcessing records for study {study_id}"
+            )
+    omics_processing_records = response.json()["cursor"]["firstBatch"]
+    logging.info(
+        f"Retrieved {len(omics_processing_records)} OmicsProcessing records for study {study_id}"
+        )
 
     # 2. For each OmicsProcessing record, retrieve the corresponding
 
