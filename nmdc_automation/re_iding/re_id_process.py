e9e7267e06ac859409df1a0a870c835bec973ec9
Michal Babinski <mbabinski@lanl.gov>
2023-10-28 07:42:22 -0700

    rename file, add database object post

--- nmdc_automation/re_iding/re_id_process.py
+++ nmdc_automation/re_iding/re_id_process.py
@@ -6,17 +6,15 @@ import yaml
 import datetime
 import pytz
 from pymongo import MongoClient
-import hashlib
 import json
-from subprocess import check_output
 from nmdc_automation.api import NmdcRuntimeApi
-from nmdc_automation.config import Config
+from re_id_file_operations import *
 import nmdc_schema.nmdc as nmdc
 from linkml_runtime.dumpers import json_dumper
 
 ###GLOBAL######
 nmdc_db = nmdc.Database()
-
+runtime_api = NmdcRuntimeApi("../../configs/site_configuration.toml")
 base = "https://data.microbiomedata.org/data"
 base_dir = "/global/cfs/cdirs/m3408/results"
 
@@ -139,23 +137,6 @@ def compute_new_paths(old_url, new_base_dir, omic_id, act_id):
     return new_url, destination, new_file_name
 
 
-def md5_sum(fn):
-    """
-    Calculate the MD5 hash of a file.
-    
-    Args:
-    - fn (str): Path to the file for which the MD5 hash is to be computed.
-    
-    Returns:
-    - str: The MD5 hash of the file.
-    """
-    with open(fn, "rb") as f:
-        file_hash = hashlib.md5()
-        while chunk := f.read(8192):
-            file_hash.update(chunk)
-    return file_hash.hexdigest()
-
-
 def find_type(obj):
     """
     Determine the data type of an object based on its URL extension.
@@ -234,80 +215,6 @@ def copy_outputs(db, outputs, omic_id, act_id):
     return new_ids, new_data_objects
 
 
-def rewrite_id(src, dst, old_id, new_id, prefix=None):
-    """
-    Rewrite lines in a file, replacing occurrences of an old ID with a new ID.
-    An optional prefix can be specified to limit which lines are modified.
-    
-    Args:
-    - src (str): Source file path.
-    - dst (str): Destination file path.
-    - old_id (str): ID to be replaced.
-    - new_id (str): Replacement ID.
-    - prefix (str, optional): Prefix character to determine which lines to modify. Defaults to None.
-    
-    Returns:
-    - tuple: MD5 checksum and size (in bytes) of the modified file.
-    """
-    fsrc = open(src)
-    fdst = open(dst, "w")
-    for line in fsrc:
-        if not prefix or (prefix and line[0] == prefix):
-            line = line.replace(old_id, new_id)
-        fdst.write(line)
-    fsrc.close()
-    fdst.close()
-    md5 = md5_sum(dst)
-    size = os.stat(dst).st_size
-    return md5, size
-
-
-def find_assembly_id(src):
-    fsrc = open(src)
-    line = fsrc.readline()
-    return "_".join(line[1:].split("_")[0:-1])
-
-
-def assembly_contigs(src, dst, omic_id, act_id):
-    scaf = src.replace("_contigs", "_scaffolds")
-    old_id = find_assembly_id(scaf)
-    return rewrite_id(src, dst, old_id, act_id, prefix=">")
-
-
-def assembly_scaffolds(src, dst, omic_id, act_id):
-    old_id = find_assembly_id(src)
-    return rewrite_id(src, dst, old_id, act_id, prefix=">")
-
-
-def assembly_coverage_stats(src, dst, omic_id, act_id):
-    scaf = src.replace("_covstats.txt", "_scaffolds.fna")
-    old_id = find_assembly_id(scaf)
-    return rewrite_id(src, dst, old_id, act_id)
-
-
-def assembly_agp(src, dst, omic_id, act_id):
-    scaf = src.replace("_assembly.agp", "_scaffolds.fna")
-    old_id = find_assembly_id(scaf)
-    return rewrite_id(src, dst, old_id, act_id)
-
-
-def convert_script(script, src, dst, old_id, act_id):
-    cmd = ["./rewrite_bam.sh", src, dst, old_id, act_id]
-    results = check_output(cmd)
-    md5 = md5_sum(dst)
-    size = os.stat(dst).st_size
-    return md5, size
-
-
-def assembly_coverage_bam(src, dst, omic_id, act_id):
-    scaf = src.replace("_pairedMapped_sorted.bam", "_scaffolds.fna")
-    old_id = find_assembly_id(scaf)
-    return convert_script("./rewrite_bam.sh", src, dst, old_id, act_id)
-
-
-def xassembly_info_file(src, dst, omic_id, act_id):
-    return []
-
 def make_activity_set(nmdc_db, omics_id, has_input, has_output,workflow_record):
     database_activity_set = getattr(nmdc_db, workflow_record["Collection"])
     # Lookup the nmdc schema range class
@@ -350,6 +257,23 @@ def make_data_object(data_object_record, omics_id):
         )
     )
 
+def post_database_object_to_runtime(datase_object):
+    
+    nmdc_database_object = json.loads(
+            json_dumper.dumps(datase_object, inject_type=False)
+        )
+    res = runtime_api.post_objects(nmdc_database_object)
+    return res
+
+def main():
+    #TODO
+    #1. Read in json dump of analysis records 
+    #2. Process records for reads qc - generate new metadata, make new records and data objects (this will include file copies and renaming)
+    #3. save data of updated reads qc records
+    #4. Fetch old records for readbased analysis and assembly, generate new metadata, make new records and data objects (this will include file copies and renaming files and ids in files)
+    #5. Validate new records, submit them via runtime api
+    #6. Write seperate process to delete old records once we have
+    pass
 
 if __name__ == "__main__":
     mongo_url = os.environ["MONGO_URL"]
