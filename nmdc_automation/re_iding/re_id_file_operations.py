219b007130c8ddb8fb15d0f32e7413f23b25a3e8
Michal Babinski <mbabinski@lanl.gov>
2023-10-28 07:42:49 -0700

    rename file, move all file operation here

--- nmdc_automation/re_iding/re_id_file_operations.py
+++ nmdc_automation/re_iding/re_id_file_operations.py
@@ -1,5 +1,98 @@
 import subprocess
 import gzip 
+import os
+import hashlib
+from subprocess import check_output
+
+def md5_sum(fn):
+    """
+    Calculate the MD5 hash of a file.
+    
+    Args:
+    - fn (str): Path to the file for which the MD5 hash is to be computed.
+    
+    Returns:
+    - str: The MD5 hash of the file.
+    """
+    with open(fn, "rb") as f:
+        file_hash = hashlib.md5()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+    return file_hash.hexdigest()
+
+def rewrite_id(src, dst, old_id, new_id, prefix=None):
+    """
+    Rewrite lines in a file, replacing occurrences of an old ID with a new ID.
+    An optional prefix can be specified to limit which lines are modified.
+    
+    Args:
+    - src (str): Source file path.
+    - dst (str): Destination file path.
+    - old_id (str): ID to be replaced.
+    - new_id (str): Replacement ID.
+    - prefix (str, optional): Prefix character to determine which lines to modify. Defaults to None.
+    
+    Returns:
+    - tuple: MD5 checksum and size (in bytes) of the modified file.
+    """
+    fsrc = open(src)
+    fdst = open(dst, "w")
+    for line in fsrc:
+        if not prefix or (prefix and line[0] == prefix):
+            line = line.replace(old_id, new_id)
+        fdst.write(line)
+    fsrc.close()
+    fdst.close()
+    md5 = md5_sum(dst)
+    size = os.stat(dst).st_size
+    return md5, size
+
+
+def find_assembly_id(src):
+    fsrc = open(src)
+    line = fsrc.readline()
+    return "_".join(line[1:].split("_")[0:-1])
+
+
+def assembly_contigs(src, dst, omic_id, act_id):
+    scaf = src.replace("_contigs", "_scaffolds")
+    old_id = find_assembly_id(scaf)
+    return rewrite_id(src, dst, old_id, act_id, prefix=">")
+
+
+def assembly_scaffolds(src, dst, omic_id, act_id):
+    old_id = find_assembly_id(src)
+    return rewrite_id(src, dst, old_id, act_id, prefix=">")
+
+
+def assembly_coverage_stats(src, dst, omic_id, act_id):
+    scaf = src.replace("_covstats.txt", "_scaffolds.fna")
+    old_id = find_assembly_id(scaf)
+    return rewrite_id(src, dst, old_id, act_id)
+
+
+def assembly_agp(src, dst, omic_id, act_id):
+    scaf = src.replace("_assembly.agp", "_scaffolds.fna")
+    old_id = find_assembly_id(scaf)
+    return rewrite_id(src, dst, old_id, act_id)
+
+
+def convert_script(script, src, dst, old_id, act_id):
+    cmd = ["./rewrite_bam.sh", src, dst, old_id, act_id]
+    results = check_output(cmd)
+    md5 = md5_sum(dst)
+    size = os.stat(dst).st_size
+    return md5, size
+
+
+def assembly_coverage_bam(src, dst, omic_id, act_id):
+    scaf = src.replace("_pairedMapped_sorted.bam", "_scaffolds.fna")
+    old_id = find_assembly_id(scaf)
+    return convert_script("./rewrite_bam.sh", src, dst, old_id, act_id)
+
+
+def xassembly_info_file(src, dst, omic_id, act_id):
+    return []
 
 def rewrite_bam(type, old_bam, new_bam, old_id, new_id):
     
