3f94d497ea49b1c086fa8a929d6044408a19d334
Michael Thornton <mbthornton@lbl.gov>
2024-01-18 12:38:17 -0800

    add METATRANSCRIPTOME activity set constant

--- nmdc_automation/re_iding/db_utils.py
+++ nmdc_automation/re_iding/db_utils.py
@@ -12,6 +12,7 @@ OMICS_PROCESSING_SET = "omics_processing_set"
 DATA_OBJECT_SET = "data_object_set"
 READS_QC_SET = "read_qc_analysis_activity_set"
 METAGENOME_ASSEMBLY_SET = "metagenome_assembly_set"
+METATRANSCRIPTOME_ACTIVITY_SET = "metatranscriptome_activity_set"
 
 
 
