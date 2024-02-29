3d59140c35f97392eee7a929d901b7c5eec1d6ac
mflynn <mflynn@lanl.gov>
2024-02-29 10:12:36 -0700

    modified workflowMonitor.js to import preprocess for ReadbasedAnalysis

--- webapp/server/crons/workflowMonitor.js
+++ webapp/server/crons/workflowMonitor.js
@@ -138,6 +138,9 @@ function generateWDL(proj_home, workflow) {
     if(workflowname === 'MetaAssembly') {
         imports += 'import "preprocess.wdl" as MetaAssembly_preprocess' + "\n";
     }
+    if(workflowname === 'ReadbasedAnalysis') {
+        imports += 'import "preprocess.wdl" as ReadbasedAnalysis_preprocess' + "\n";
+    }
     const tmpl = process.env.WORKFLOW_TEMPLATE_HOME + "/" + workflowSettings['wdl_tmpl'];
     let templWDL = String(fs.readFileSync(tmpl));
     templWDL = templWDL.replace(/<WORKFLOW>/g, workflowname);
