5810d085c79d57be7c775a41177011942b971a7c
Michael Thornton <mbthornton@lbl.gov>
2024-07-17 09:25:57 -0700

    delete files after re-IDing and rewriting

--- nmdc_automation/re_iding/scripts/bug_fix.py
+++ nmdc_automation/re_iding/scripts/bug_fix.py
@@ -34,6 +34,11 @@ def fix_malformed_assembly_paths(expected_paths_file, production, update_files, 
     else:
         logging.basicConfig(level=logging.INFO)
 
+    if production:
+        datafile_dir = PROD_DATAFILE_DIR
+    else:
+        datafile_dir = LOCAL_DATAFILE_DIR
+
     logger.info(f"Expected paths file: {expected_paths_file}")
     logger.info(f"Production: {production}")
     logger.info(f"Update files: {update_files}")
@@ -41,11 +46,7 @@ def fix_malformed_assembly_paths(expected_paths_file, production, update_files, 
     # Read the expected paths file
     with open(expected_paths_file) as f:
 
-        if production:
-            datafile_dir = PROD_DATAFILE_DIR
-        else:
-            datafile_dir = LOCAL_DATAFILE_DIR
-
+        num_processed = 0
         for line in f:
             # Parse out the components of the path
             line = line.strip()
@@ -63,76 +64,97 @@ def fix_malformed_assembly_paths(expected_paths_file, production, update_files, 
                 continue
 
             # Search for the malformed assembly directory
-            assembly_dirname = None
+            assembly_id = None
             for dir in omics_data_dir.iterdir():
                 if exp_assembly_id in dir.name:
-                    assembly_dirname = dir.name
-                    working_dir = omics_data_dir.joinpath(assembly_dirname)
+                    assembly_id = dir.name
+                    working_dir = omics_data_dir.joinpath(assembly_id)
                     break
-            if assembly_dirname is None:
+            if assembly_id is None:
                 logger.error(f"Assembly directory not found: {exp_assembly_id}")
                 continue
-            logger.info(f"Found assembly directory: {assembly_dirname}")
+            logger.info(f"Found assembly directory: {assembly_id}")
 
             # Rename the assembly directory to the expected name if it does not match and update_files is True
-            if assembly_dirname != exp_assembly_id:
+            if assembly_id != exp_assembly_id:
                 new_assembly_dir = omics_data_dir.joinpath(exp_assembly_id)
                 if update_files:
-                    working_dir.rename(new_assembly_dir)
-                    logger.info(f"Renamed assembly directory: {working_dir} to {new_assembly_dir}")
+                    working_dir = working_dir.rename(new_assembly_dir)
+                    logger.info(f"Renamed assembly directory: {working_dir}")
                 else:
-                    logger.info(f"Would rename assembly directory: {assembly_dirname} to {exp_assembly_id}")
+                    logger.info(f"Would rename assembly directory: {assembly_id} to {exp_assembly_id}")
 
             # Data Files
-            # need to break up expected and actual file names into components
-            # e.g nmdc_wfmgas-11-nvt77985.1.1_pairedMapped_sorted.bam
-            _nmdc, exp_datafile_name, exp_datafile_type = exp_filename.split("_", maxsplit=2)
-            # Search for the data file
-            datafile = None
-            logger.debug(f"exp_assembly_id: {exp_assembly_id} exp_datafile_type: {exp_datafile_type}")
-            for file in working_dir.iterdir():
-                logger.debug(f"file: {file}")
-                if exp_datafile_name in file.name and exp_datafile_type in file.name:
-                    datafile = file
-                    break
-            if datafile is None:
-                logger.error(f"Data file not found: {exp_filename}")
+            # Parse the expected data file name
+            _nmdc, exp_datafile_assembly_id, exp_datafile_type = exp_filename.split("_", maxsplit=2)
+            # Search for the malformed data file(s) - check all files as the expected_paths_file is not comprehensive
+            datafiles = list(working_dir.glob(f"*{exp_datafile_assembly_id}*"))
+            if len(datafiles) == 0:
+                logger.error(f"Data files not found for: {exp_datafile_assembly_id}")
                 continue
-            logger.info(f"Found data file: {datafile}")
+            logger.info(f"Found {len(datafiles)} data files for: {exp_datafile_assembly_id}")
+            for datafile in datafiles:
+                logger.info(f"Examining: {datafile}")
+                # Parse the data file name
+                _nmdc, datafile_assembly_id, datafile_type = datafile.name.split("_", maxsplit=2)
 
-            # Infer the data type from the data file name and extension
-            if datafile.name.endswith("pairedMapped_sorted.bam"):
-                data_type = "Assembly Coverage BAM"
-            elif datafile.name.endswith("assembly.agp"):
-                data_type = "Assembly AGP"
-            elif datafile.name.endswith("_scaffolds.fna"):
-                data_type = "Assembly Scaffolds"
-            elif datafile.name.endswith("_contigs.fna"):
-                data_type = "Assembly Contigs"
-            elif datafile.name.endswith("mapping_stats.txt"):
-                data_type = "Assembly Coverage Stats"
-            else:
-                logger.error(f"Unknown data file type: {datafile.name}")
-                continue
+                # exact match - no need to rename
+                if datafile_assembly_id == exp_datafile_assembly_id and datafile_type == exp_datafile_type:
+                    logger.info(f"Data file name matches expected: {datafile.name}")
+                    continue
+                # datafile names match - assume no need to rename
+                if datafile_assembly_id == exp_datafile_assembly_id:
+                    logger.info(f"Data file name matches assembly id: {datafile.name}")
+                    continue
+
+                # datafile names do not match - rename
+                old_workflow_id = assembly_id
+                new_workflow_id = exp_assembly_id
+                data_object_type = _infer_data_object_type_from_name(datafile.name)
+                old_file_path = datafile
+                new_file_path = working_dir.joinpath(f"nmdc_{new_workflow_id}_{datafile_type}")
+                num_processed += 1
+                logger.info(f"Data file name does not match expected: {datafile.name}")
+                logger.info(f"Data object type: {data_object_type}")
+                logger.info(f"Old path: {old_file_path}")
+                logger.info(f"New path: {new_file_path}")
+
+                if update_files:
+                    if not data_object_type:
+                        logger.error(f"Do not know how to update with no data object type: {datafile.name}")
+                        continue
+                    md5, size = assembly_file_operations(
+                        old_workflow_id, new_workflow_id, data_object_type, old_file_path, new_file_path)
+                    logger.info(f"New path: {new_file_path}")
+                    logger.info(f"MD5: {md5}, Size: {size}")
+
+                    # Remove the old file
+                    old_file_path.unlink()
+
+                else:
+                    logger.info("--update-files not set.")
+                    logger.info(f"Would update to {new_file_path}")
+
+    logger.info(f"Processed {num_processed} files in {time.time() - start_time} seconds.")
 
 
-            old_workflow_id = assembly_dirname
-            new_workflow_id = exp_assembly_id
-            data_object_type = data_type
-            old_file_path = datafile
-            new_file_path = working_dir.joinpath(exp_filename)
 
-            logger.info(f"old_workflow_id: {old_workflow_id}")
-            logger.info(f"new_workflow_id: {new_workflow_id}")
-            logger.info(f"data_object_type: {data_object_type}")
-            logger.info(f"old_file_path: {old_file_path}")
-            logger.info(f"new_file_path: {new_file_path}")
-
-
-
-
-
-
+def _infer_data_object_type_from_name(name: str) -> str:
+    # Infer the data type from the data file name and extension
+    data_type = None
+    if name.endswith("pairedMapped_sorted.bam"):
+        data_type = "Assembly Coverage BAM"
+    elif name.endswith("assembly.agp"):
+        data_type = "Assembly AGP"
+    elif name.endswith("_scaffolds.fna"):
+        data_type = "Assembly Scaffolds"
+    elif name.endswith("_contigs.fna"):
+        data_type = "Assembly Contigs"
+    elif name.endswith("mapping_stats.txt") or name.endswith("_covstats.txt"):
+        data_type = "Assembly Coverage Stats"
+    else:
+        logger.error(f"Unknown data file type: {name}")
+    return data_type
 
 
 
