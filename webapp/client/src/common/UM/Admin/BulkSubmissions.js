b63bd0b8084fd807508f60bbd1cb9e476a0ae712
Yan Xu <yxu@pn1704854.lanl.gov>
2024-12-16 15:25:48 -0700

    Update text

--- webapp/client/src/common/UM/Admin/BulkSubmissions.js
+++ webapp/client/src/common/UM/Admin/BulkSubmissions.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Col, Row, } from 'reactstrap';
+import { Badge, Col, Row, } from 'reactstrap';
 import BulkSubmissionTable from '../Common/BulkSubmissionTable';
 
 function BulkSubmissions(props) {
     return (
         <div className="animated fadeIn">
+            <Row >
+                <Col xl={1}></Col>
+                <Col xl={10}>
+                    <div className="clearfix">
+                        <Badge href="#" color="danger" pill>Admin tool</Badge>
+                    </div>
+                </Col>
+            </Row>
+            <br></br>
             <Row>
                 <Col xl={1}></Col>
                 <Col xl={10}>
-                    <BulkSubmissionTable tableType='admin' title={"Manage Submissions"} {...props} />
+                    <BulkSubmissionTable tableType='admin' title={"Manage Bulk Submissions"} {...props} />
                 </Col>
             </Row>
             <br></br>
