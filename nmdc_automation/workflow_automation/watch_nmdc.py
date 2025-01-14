46d117b4fb7195ae46b7eeea667352a51e89d3c1
Michael Thornton <mbthornton@lbl.gov>
2025-01-14 12:13:20 -0800

    add opid to status output line

--- nmdc_automation/workflow_automation/watch_nmdc.py
+++ nmdc_automation/workflow_automation/watch_nmdc.py
@@ -155,7 +155,7 @@ class JobManager:
                 # already in cache
                 continue
             wf_job = WorkflowJob(self.config, workflow_state=job)
-            logger.info(f"Job from State: {wf_job.was_informed_by} / {wf_job.workflow_execution_id}, Last Status: {wf_job.workflow.last_status} / {wf_job.workflow.nmdc_jobid}")
+            logger.info(f"Job from State: {wf_job.was_informed_by} / {wf_job.workflow_execution_id}, Last Status: {wf_job.workflow.last_status} /{wf_job.opid} / {wf_job.workflow.nmdc_jobid}")
             job_cache_ids.append(wf_job.opid)
             wf_job_list.append(wf_job)
 
