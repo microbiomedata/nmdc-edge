bff2edb30391be5054e8e27252e4e404e907307a
Shalki Shrivastava <Shalsh23@users.noreply.github.com>
2025-02-10 22:44:25 +0530

    Update jamo_ingest.py

--- nmdc_automation/jamo_ingest.py
+++ nmdc_automation/jamo_ingest.py
@@ -1,16 +1,46 @@
-valid_data.json = "/path/to/valid_data.json"
+import json
 
-for record in valid_records.json
-    url = record['url']
-    file = record['name']
-    data_object_id = record['id']
-    label = record['data_object_type']
+valid_data = "valid_data.json"
 
-    prefix = "https://data.microbiomedata.org/data/"
-    url_suffix = url.removeprefix(prefix)
-    tokens = url_suffix.split('/')
-    
-    was_informed_by = tokens[0]
-    workflow_execution_id = tokens[1]
-    workflow_execution = workflow_execution_id.split('-')[0].removeprefix('nmdc_')
-    file_format = file.split('.')[-1]
+with open("valid_data.json") as f:
+	valid_data = json.load(f)
+
+	for record in valid_data:
+		url = record['url']
+	    file = record['name']
+	    data_object_id = record['id']
+	    label = record['data_object_type']
+
+	    prefix = "https://data.microbiomedata.org/data/"
+	    url_suffix = url.removeprefix(prefix)
+	    tokens = url_suffix.split('/')
+	    
+	    was_informed_by = tokens[0]
+	    workflow_execution_id = tokens[1]
+	    workflow_execution = workflow_execution_id.split('-')[0].removeprefix('nmdc_')
+	    file_format = file.split('.')[-1]
+
+
+	# create json structure
+
+	json_data = {
+	    "metadata": {
+	        "workflow_execution": workflow_execution,
+	        "workflow_execution_id": workflow_execution_id,
+	        "data_object_id": data_object_id,
+	        "was_informed_by": was_informed_by
+	    },
+	    "outputs": [
+	        {
+	            "file": file,
+	            "label": label,
+	            "metadata": {
+	                "file_format": file_format
+	            }
+	        }
+	        
+	    ]
+	}
+
+	with open('metadata.json', w) as f:
+		json.dump(json_data, f, indent=4)
