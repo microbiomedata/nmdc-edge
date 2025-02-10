1c6bd9b3d58661cef2016f264e65660ff74443dc
Michael Thornton <mbthornton@lbl.gov>
2025-02-10 14:07:09 -0800

    update-zero-size-files command find and try to size files

--- nmdc_automation/run_process/db_tools.py
+++ nmdc_automation/run_process/db_tools.py
@@ -1,11 +1,12 @@
 """
 DB Tools.
 """
-import click
 import logging
+import os
 
-from nmdc_automation.api import NmdcRuntimeApi
+import click
 
+from nmdc_automation.config import SiteConfig
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -22,4 +23,46 @@ def cli():
 def update_zero_size_files(config_file, update_db):
     logger.info(f"Updating zero size files from {config_file}")
 
-    runtime = NmdcRuntimeApi(config_file)
+    site_config = SiteConfig(config_file)
+    url_root = site_config.url_root
+    headers = {'accept': 'application/json', }
+
+    import requests
+
+    headers = {'accept': 'application/json', }
+
+    params = {
+        'filter': '{"$or": [{"file_size_bytes": {"$exists": false}},{"file_size_bytes": null},{"file_size_bytes": 0}],"$and": [{"url": {"$exists": true}},{"url": {"$regex": "^https://data.microbiomedata.org/data/"}}]}',
+        'max_page_size': '10000', }
+
+    response = requests.get(
+        'https://api-dev.microbiomedata.org/nmdcschema/data_object_set', params=params, headers=headers
+    )
+
+    response = requests.get(
+        'https://api-dev.microbiomedata.org/nmdcschema/data_object_set', params=params, headers=headers
+    )
+    if response.status_code != 200:
+        logger.error(f"Error in response: {response.status_code}")
+        return
+    data_objects = response.json().get("resources", [])
+
+    logger.info(f"Found {len(data_objects)} data objects with zero size data files")
+
+    for dobj in data_objects:
+        # get everything after 'data/' in the url
+        file_path = dobj['url'].split('data/')[1]
+        file_path = os.path.join(site_config.data_dir, file_path)
+
+        logger.info(f"Updating {dobj['id']} /  File size: {dobj.get('file_size_bytes', None)} / File: {file_path}")
+        # try to get the file size in bytes
+        try:
+            file_size = os.path.getsize(file_path)
+            logger.info(f"File size: {file_size}")
+        except FileNotFoundError:
+            logger.error(f"File not found: {file_path}")
+            continue
+
+
+if __name__ == "__main__":
+    cli()
