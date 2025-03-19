86c13a037acc2ba12c3340564b3be5085ff064b0
Michael Thornton <mbthornton@lbl.gov>
2025-03-19 16:06:52 -0700

    try with shell_check set to false

--- tests/test_jaws_api_integration.py
+++ tests/test_jaws_api_integration.py
@@ -47,7 +47,7 @@ def test_jaws_job_runner_submit_job(site_config, fixtures_dir, jaws_token_file, 
 
     runner = JawsRunner(site_config, state_manager, jaws_api)
     submission_files = runner.generate_submission_files()
-    validation_resp = jaws_api.validate(shell_check=True, wdl_file=submission_files["wdl_file"],
+    validation_resp = jaws_api.validate(shell_check=False, wdl_file=submission_files["wdl_file"],
                                         inputs_file=submission_files["inputs"])
     print(validation_resp)
     assert validation_resp["result"] == "succeeded"
