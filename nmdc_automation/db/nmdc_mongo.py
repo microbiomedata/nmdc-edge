41834f2170207cf26f785661994cc3779fc0f6f1
Michael Thornton <mbthornton@lbl.gov>
2025-05-02 11:15:02 -0700

    Refactor get_mongo_db to nmdc_automation/db
    Move this package to the top level so we can remove redundancies in file staging

---
+++ nmdc_automation/db/nmdc_mongo.py
@@ -0,0 +1,17 @@
+import os
+from functools import lru_cache
+
+from pymongo import MongoClient
+from pymongo.synchronous.database import Database as MongoDatabase
+
+
+@lru_cache
+def get_mongo_db() -> MongoDatabase:
+    _client = MongoClient(
+        host=os.getenv("MONGO_HOST", "localhost"),
+        port=int(os.getenv("MONGO_PORT", "27018")),
+        username=os.getenv("MONGO_USERNAME", None),
+        password=os.getenv("MONGO_PASSWORD", None),
+        directConnection=True,
+    )[os.getenv("MONGO_DBNAME", "nmdc")]
+    return _client
