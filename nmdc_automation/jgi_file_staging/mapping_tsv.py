37196275a7396c30557a5e437f68c7fc4da33f4d
mflynn <mflynn@lanl.gov>
2025-06-05 13:04:45 -0600

    added docstrings, changed default file location to directory with staged files

--- nmdc_automation/jgi_file_staging/mapping_tsv.py
+++ nmdc_automation/jgi_file_staging/mapping_tsv.py
@@ -2,17 +2,19 @@ import logging
 import pathlib
 import sys
 
+import pandas
 import pandas as pd
 from pathlib import Path
 from argparse import ArgumentParser
+from pymongo.database import Database
 
 from nmdc_automation.db.nmdc_mongo import get_db
 from nmdc_automation.jgi_file_staging.jgi_file_metadata import get_access_token, get_request
 
 
-def create_mapping_tsv(project_name: str, mapping_file_path: pathlib.Path, mdb, study_id) -> None:
+def create_mapping_tsv(project_name: str, mapping_file_path: pathlib.Path, mdb: Database, study_id: str) -> None:
     """
-    Creates mapping tsv file for a given project
+    Creates mapping tsv file(s) for a given project
     :param project_name: Name of the project
     :param mapping_file_path: path where to save the mapping tsv file
     :param study_id: study id of the project that will be used to get the proposal id
@@ -26,31 +28,41 @@ def create_mapping_tsv(project_name: str, mapping_file_path: pathlib.Path, mdb, 
     study_df = study_df.apply(
         lambda x: get_gold_analysis_project(x, ACCESS_TOKEN), axis=1)
     study_df = study_df.loc[pd.notna(study_df.gold_analysis_project), :]
-    metat_study_df = study_df.loc[study_df.ap_type == 'Metatranscriptome Analysis', ['id', 'gold_analysis_project']]
-    new_row_list = []
-    for idx, row in metat_study_df.iterrows():
-        new_row_list.append({'id': row.id, 'gold_analysis_project': row.gold_analysis_project[0]})
-        new_row_list.append({'id': row.id, 'gold_analysis_project': row.gold_analysis_project[1]})
-    metat_study_df = pd.DataFrame(new_row_list)
+
     metag_study_df = study_df.loc[study_df.ap_type == 'Metagenome Analysis', ['id', 'gold_analysis_project']]
-    create_tsv_file(metag_study_df, mapping_file_path, project_name, 'metag', mdb)
+    create_tsv_file(metag_study_df, project_name, 'metag', mdb, mapping_file_path)
+    metat_study_df = study_df.loc[study_df.ap_type == 'Metatranscriptome Analysis', ['id', 'gold_analysis_project']]
     if not metat_study_df.empty:
-        create_tsv_file(metat_study_df, mapping_file_path, project_name, 'metat', mdb)
+        new_row_list = []
+        for idx, row in metat_study_df.iterrows():
+            new_row_list.append({'id': row.id, 'gold_analysis_project': row.gold_analysis_project[0]})
+            new_row_list.append({'id': row.id, 'gold_analysis_project': row.gold_analysis_project[1]})
+        metat_study_df = pd.DataFrame(new_row_list)
+        create_tsv_file(metat_study_df, project_name, 'metat', mdb, mapping_file_path)
 
 
-def create_tsv_file(study_df, mapping_file_path, project_name, ap_type, mdb):
-
+def create_tsv_file(study_df: pandas.DataFrame, project_name: str, ap_type: str, mdb: Database,
+                    mapping_file_path: str = None):
+    """
+    Create mapping tsv file for either metaG or metaT analysis projects
+    :param study_df: pandas DataFrame containing gold analysis records
+    :param project_name: Name of the project
+    :param ap_type: type of analysis project
+    :param mdb: MongoDB database object
+    :param mapping_file_path: path where to save the mapping tsv file
+    """
     sequencing_project = mdb.sequencing_projects.find_one({'project_name': project_name})
+    mapping_file_path = Path(sequencing_project['analysis_projects_dir'], sequencing_project['project_name']) if not mapping_file_path else mapping_file_path
     study_df['project_path'] = study_df.apply(lambda x: str(
         Path(sequencing_project['analysis_projects_dir'], sequencing_project['project_name'],
-             f"{sequencing_project['project_name']}_analysis_projects", x['gold_analysis_project']) if x[
+             f"analysis_files", x['gold_analysis_project']) if x[
             'gold_analysis_project'] else None), axis=1)
     study_df = study_df[['id', 'gold_analysis_project', 'project_path']]
     study_df.columns = ['nucleotide_sequencing_id', 'project_id', 'project_path']
     study_df.to_csv(Path(mapping_file_path, f'{project_name}.{ap_type}.map.tsv'), sep='\t', index=False)
 
 
-def get_study_id(project_name, ACCESS_TOKEN, mdb):
+def get_study_id(project_name: str, ACCESS_TOKEN: str, mdb: Database) -> str:
     """
     Given a proposal_id for a project, return the corresponding NMDC study id
     """
@@ -65,7 +77,7 @@ def get_study_id(project_name, ACCESS_TOKEN, mdb):
         return None
 
 
-def get_gold_ids(nmdc_study_id, ACCESS_TOKEN):
+def get_gold_ids(nmdc_study_id: str, ACCESS_TOKEN: str) -> pd.DataFrame:
     """
     Find the gold ids for a given NMDC study by querying the nmdc schema data_generation_set endpoint
     """
@@ -77,7 +89,11 @@ def get_gold_ids(nmdc_study_id, ACCESS_TOKEN):
     return study_df.loc[pd.notna(study_df['gold_sequencing_project_identifiers']), :]
 
 
-def get_gold_analysis_project(row, ACCESS_TOKEN):
+def get_gold_analysis_project(row: pd.Series, ACCESS_TOKEN: str) -> pd.Series:
+    """
+    get gold analysis project id for gold project id
+    param: row: pandas Series containing gold analysis record
+    """
     url = f"https://gold-ws.jgi.doe.gov/api/v1/analysis_projects?projectGoldId={row['gold_project']}"
     study_samples_json = get_request(url, ACCESS_TOKEN)
     if not study_samples_json:
@@ -99,7 +115,9 @@ def get_gold_analysis_project(row, ACCESS_TOKEN):
                                          ((analysis_df.apType == mapping_type) &
                                           (analysis_df.referenceApGoldId == ap_gold_id)), 'apGoldId'].values
         elif ap_type == 'Metagenome Analysis' and len(analysis_df[(analysis_df.apType == 'Metagenome Analysis')]) > 1:
-            return None, ap_type
+            row['gold_analysis_project'] = None
+            row['ap_type'] = ap_type
+            return row
         row['gold_analysis_project'] = ap_gold_id
         row['ap_type'] = ap_type
     return row
@@ -109,7 +127,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('project_name')
     parser.add_argument('-s', '--study_id', default=None)
-    parser.add_argument('-f', '--file_path', default='.')
+    parser.add_argument('-f', '--file_path', default=None)
     args = vars((parser.parse_args()))
 
     # Get the database connection
